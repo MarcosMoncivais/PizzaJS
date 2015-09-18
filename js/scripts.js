a56fde135573f3e4d4c26fd055e707c75ef4269d
Marcos Moncivais <marcos.moncivais@gmail.com>
2015-09-18 14:07:44 -0700

    get tests passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,32 @@
-//check to see if word shows in user input form get-go
-var find = function (wordOne, wordTwo) {
-	if (wordOne === wordTwo) {
-		return true;
-} else {
-	return false;
-	}
-};
-
-//OMG JUST GOT THIS PASSING
-	var findReplace = function(sentence, wordFind, wordReplace) {
-	var UserInput = sentence.split(" ");
-	for (var i = 0; i < UserInput.length; i++) {
-		if (UserInput[i] === wordFind) {
-	  		UserInput[i] = wordReplace;
-		}
-	}
-	var resultString = UserInput.join(" ");
-  //check if changes were made
-  if (resultString === sentence) {
-    return "nope!";
-  } else {
-    return resultString;
+function Pizza(size, toppings, quantity){
+  this.size = size;
+  this.toppings = toppings;
+  this.quantity = quantity;
+}
+Pizza.prototype.price = function(){
+  var price = 0 + (this.toppings + 5);
+  if(this.size === "Small"){
+   return price + 8 * this.quantity;
+  } else if(this.size === "Medium" ){
+   return price + 10 * this.quantity;
+  } else if(this.size === "Large"){
+    price + 12 * this.quantity;
   }
 };
 
-$(document).ready(function(){
-  $("form#findReplace").submit(function(event) {
-    var sentence = $("input#sentence").val();
-    var wordFind = $("input#wordFind").val();
-    var wordReplace = $("input#wordReplace").val();
-    
-    var newSentence = findReplace(sentence, wordFind, wordReplace);
-    
-    $(".resultString").show();
-    event.preventDefault();
-  });
+$(document).ready(function() {
+    $("form#order").submit(function(event) {
+        event.preventDefault();
+        var size = $("input#size").val();
+        var toppings = $("input#toppings").val();
+        var quantity = $("input#quantity").val();
 
+        var newPizza = new Pizza(size, toppings, quantity);
+
+        $(".size").text(newPizza.size);
+        $(".toppings").text(toppings);
+        $(".quantity").text(newPizza.quantity);
+        $(".price").text(newPizza.price());
+        $("#show-order").show();
+    });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,24 @@
-describe("find", function() {
-	it('checks to see if two words match', function(){
-		expect(find("poop", "poop")).to.equal(true)
+describe("Pizza", function(){
+	it("creates a pizza with given specifications", function(){
+		var testPizza = new Pizza("Medium", 1, 1);
+    expect(testPizza.size).to.equal("Medium");
+    expect(testPizza.toppings).to.equal(1);
+    expect(testPizza.quantity).to.equal(1);
 	});
-});
+	
+	it("will price pizza according to size", function (){
+		var testPizza = new Pizza("Small", 1, 1);
+		expect(testPizza.price()).to.equal(14);
+	});
+	
+	it("will check to see if user added toppings and adjust price accordingly", function(){
+		var testPizza = new Pizza("Medium", 1, 2);
+		expect(testPizza.price()).to.equal(26);
+	});
+	
+	it("will price pizza according to quantity", function(){
+		var testPizza = new Pizza("Small", 1, 3);
+		expect(testPizza.price()).to.equal(30);
+	});
+});	
 
-describe("findReplace", function () {
-  it("will find all instances of a word in a string and replace it with another", function () {
-    expect(findReplace("hi mom wsup", "mom", "dad")).to.equal("hi dad wsup");
-  });
-
-  it("replaces a word that occurs multiple times in a sentence", function () {
-    expect(findReplace("i love love you", "love", "hate")).to.equal("i hate hate you");
-  });
-});
