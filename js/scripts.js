41d80a4d15ea6d81f87cd33545b4ff284dfd5b32
Marcos Moncivais <marcos.moncivais@gmail.com>
2015-09-18 15:06:14 -0700

    finish html file, try to fix output error

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,34 @@ function Pizza(size, toppings, quantity){
   this.toppings = toppings;
   this.quantity = quantity;
 }
-debugger;
-Pizza.prototype.price = function(){
+Pizza.prototype.pizzaPrice = function(){
   var price = 0 + (this.toppings + 5);
   if(this.size === "Small"){
-   return price + 8 * this.quantity;
-  } else if(this.size === "Medium" ){
-   return price + 10 * this.quantity;
-  } else if(this.size === "Large"){
-    price + 12 * this.quantity;
+   return price + 8 + this.toppings * this.quantity;
+  } else 
+  
+  if(this.size === "Medium" ){
+   return price + 10 + this.toppings * this.quantity;
+  } else 
+  
+  if(this.size === "Large"){
+   return price + 12 + this.toppings * this.quantity;
   }
 };
 
-$(document).ready(function() {
-    $("form#order").submit(function(event) {
-        event.preventDefault();
-        var size = $("input#size").val();
-        var toppings = $("input#toppings").val();
-        var quantity = $("input#quantity").val();
+// $(document).ready(function() {
+//     $("form#order").submit(function(event) {
+//         event.preventDefault();
+//         var size = $("input#size").val();
+//         var toppings = $("input#toppings").val();
+//         var quantity = $("input#quantity").val();
 
-        var newPizza = new Pizza(size, toppings, quantity);
+//         var newPizza = new Pizza(size, toppings, quantity);
 
-        $(".size").text(newPizza.size);
-        $(".toppings").text(toppings);
-        $(".quantity").text(newPizza.quantity);
-        $(".price").text(newPizza.price());
-        $("#show-order").show();
-    });
-});
+//         $(".size").text(newPizza.size);
+//         $(".toppings").text(toppings);
+//         $(".quantity").text(newPizza.quantity);
+//         $(".price").text(newPizza.price());
+//         $("#show-order").show();
+//     });
+// });
