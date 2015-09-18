15d5c44377acb6d5ee193c482fcdd66721b53fae
Marcos Moncivais <marcos.moncivais@gmail.com>
2015-09-18 15:41:27 -0700

    tried, can't get it

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,16 @@ function Pizza(size, toppings, quantity){
   this.toppings = toppings;
   this.quantity = quantity;
 }
-debugger;
-Pizza.prototype.pizzaPrice = function(){
-  var totalPrice = 0 + (this.toppings + 5);
-  if(this.size === "Small"){
-   return price + 8 + this.toppings * this.quantity;
-  } else if(this.size === "Medium" ){
-   return price + 10 + this.toppings * this.quantity;
-  } else if(this.size === "Large"){
-   return price + 12 + this.toppings * this.quantity;
-  }
-};
+    Pizza.prototype.pizzaPrice = function(){
+    var totalPrice = 0 + (this.toppings + 5);
+      if(this.size === "Small"){
+      return price + 8 + this.toppings * this.quantity;
+      } else if(this.size === "Medium" ){
+      return price + 10 + this.toppings * this.quantity;
+      } else if(this.size === "Large"){
+      return price + 12 + this.toppings * this.quantity;
+      }
+    };
 
 $(document).ready(function() {
     $("form#order-form").submit(function(event) {
@@ -27,6 +26,6 @@ $(document).ready(function() {
        $("#price").text(totalPrice);
        $("#result").show();
        
-               event.preventDefault();
+       event.preventDefault();
     });
 });
